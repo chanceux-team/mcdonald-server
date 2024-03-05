1b0d4e9bd7b802e37f4da42fbbe1532ad8ea2aa3
qianran <qianran@beansmile.com>
2024-03-05 17:24:59 +0800

    feat: learn

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CalendarModule } from './calendar/calendar.module';
 import { BotModule } from './bot/bot.module';
+import { ActionModule } from './action/action.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     CalendarModule,
     BotModule,
+    ActionModule,
   ],
   controllers: [],
   providers: []
