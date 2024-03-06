64ffaa6c49d0395f1c090dcc4803c586ce8ef141
qianran <qianran@beansmile.com>
2024-03-06 15:02:50 +0800

    fix: 输入命令生成了 2 张图 & 接口请求改成调用calendarService的接口

--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -1,16 +1,17 @@
 import { Module } from '@nestjs/common';
 import { BotController } from './bot.controller';
 import { BotService } from './bot.service';
-import { PrismaService } from '../shared/services/prisma.service';
 import { ConfigModule } from '@nestjs/config';
+import { CalendarModule } from '../calendar/calendar.module'
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    CalendarModule,
   ],
   controllers: [BotController],
   providers: [
     BotService,
   ],
 })
-export class BotModule { }
+export class BotModule {}
