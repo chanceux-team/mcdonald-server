b14af1cb3a59cd1e8470087075f74c6c04b6be86
qianran <qianran@beansmile.com>
2024-03-07 18:31:24 +0800

    feat: 输入指令增加统计数据

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -4,6 +4,8 @@ import puppeteer from 'puppeteer';
 import { FormData } from "formdata-node"
 import { Blob } from 'buffer';
 import { CalendarService } from '../calendar/calendar.service'
+import type { CalendarQueryDto } from '../calendar/dto/index.dto'
+import * as dayjs from 'dayjs';
 
 @Injectable()
 export class BotService {
@@ -26,6 +28,77 @@ export class BotService {
     } else {
       return { message: 'Element not found' };
     }
+  }
+
+  /**
+   * 获取日期段的打开次数
+   */
+  async getCountSum(query: CalendarQueryDto): Promise<number> {
+    return await this.calendarService.getCountSum(query);
+  }
+
+  async post(post: any, action?: string) {
+    const { file: buffer } = await this.capture();
+    const body = new FormData();
+    const fileName = `${new Date().toISOString().replace(/:/g, '-')}.png`;
+    body.append('files', new Blob([buffer]), fileName);
+    const response = await fetch(
+      `https://${process.env.MM_DOMAIN}/api/v4/files?channel_id=${post.channel_id}&filename=${fileName}`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${process.env.BOT_TOKEN}`,
+        },
+        body: body,
+      },
+    );
+
+    const file = await response.json();
+
+    const [todayCount, weekCount, monthCount] = await Promise.all([
+      this.getCountSum({
+        start_date: dayjs().format('YYYY-MM-DD'),
+        end_date: dayjs().format('YYYY-MM-DD'),
+      }),
+      this.getCountSum({
+        start_date: dayjs().startOf('week').format('YYYY-MM-DD'),
+        end_date: dayjs().format('YYYY-MM-DD'),
+      }),
+      this.getCountSum({
+        start_date: dayjs().startOf('month').format('YYYY-MM-DD'),
+        end_date: dayjs().format('YYYY-MM-DD'),
+      }),
+    ])
+
+    const message = `已为你成功${action}！`;
+    const analytics = `今天累计打卡${todayCount}次，本周累计打卡${weekCount}次， 本月累计打卡${monthCount}次`;
+    fetch(`https://${process.env.MM_DOMAIN}/api/v4/posts`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.BOT_TOKEN}`,
+      },
+      body: JSON.stringify({
+        channel_id: post.channel_id,
+        // root_id: post.id,
+        message: '## 麦当劳打卡统计\n' + (action ? message : '') + '\n' + analytics,
+        file_ids: file.file_infos.map((f) => f.id),
+      }),
+    });
+  }
+
+  /**
+   * 从消息中提取操作数字
+   * @param message 消息
+   */
+  extract(message: string): { operation: '+' | '-'; count: number } {
+    const match = message.match(/@mcdonalds (\S)(\d+)/);
+    if (match) {
+      const operation = match[1] as '+' | '-';
+      const count = Number(match[2]);
+      return { operation, count };
+    }
+    return null;
   }
 
   // 初始化 WebSocket 连接
@@ -52,39 +125,15 @@ export class BotService {
       const post = JSON.parse(data.data.post);
       if (!post.message || !post.message.startsWith('@mcdonalds')) return
 
-      const count = Number(post.message.match(/@mcdonalds ([+-]?\d+)/)?.[1]) || undefined;
-      if (/@mcdonalds [+-]?\d+/.test(post.message)) {
-        await this.calendarService.updateCount({ count })
+      const { count, operation } = this.extract(post.message) || {};
+
+      if (count && operation) {
+        await this.calendarService.updateCount({ count: Number(`${operation}${count}`) })
+        this.post(post, `${operation}${count}`)
+        return
       }
-
-
-      const { file: buffer } = await this.capture();
-      const body = new FormData();
-      const fileName = `${new Date().toISOString().replace(/:/g, '-')}.png`;
-      body.append('files', new Blob([buffer]), fileName);
-      const response = await fetch(
-        `https://${process.env.MM_DOMAIN}/api/v4/files?channel_id=${post.channel_id}&filename=${fileName}`,
-        {
-          method: 'POST',
-          headers: {
-            Authorization: `Bearer ${process.env.BOT_TOKEN}`,
-          },
-          body: body,
-        },
-      );
-      const file = await response.json();
-      fetch(`https://${process.env.MM_DOMAIN}/api/v4/posts`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${process.env.BOT_TOKEN}`,
-        },
-        body: JSON.stringify({
-          channel_id: post.channel_id,
-          // root_id: post.id,
-          file_ids: file.file_infos.map((f) => f.id),
-        }),
-      });
+      this.post(post)
     });
   }
 }
+
--- src/calendar/calendar.controller.ts
+++ src/calendar/calendar.controller.ts
@@ -1,7 +1,7 @@
-import { Get, Controller, Param, Post, Body } from '@nestjs/common';
+import { Get, Controller, Param, Post, Body, Query } from '@nestjs/common';
 import { CalendarService } from './calendar.service';
 import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags, ApiBody, ApiConsumes } from '@nestjs/swagger';
-import { CalendarUpdateDto } from './dto/index.dto';
+import { CalendarUpdateDto, CalendarQueryDto } from './dto/index.dto';
 import type { Calendar } from './interface'
 
 @ApiBearerAuth()
@@ -10,10 +10,16 @@ import type { Calendar } from './interface'
 export class CalendarController {
   constructor(private readonly service: CalendarService) { }
 
-  @ApiOperation({ summary: '获取一年内的数据' })
+  @ApiOperation({ summary: '获取Calendar数据（默认一年内）' })
   @Get()
-  async getData(): Promise<any[]> {
+  async getData(): Promise<Calendar[]> {
     return await this.service.get();
+  }
+
+  @ApiOperation({ summary: '获取时间段的累加数据' })
+  @Get('counts')
+  async getCountSum(@Query() query: CalendarQueryDto): Promise<number> {
+    return await this.service.getCountSum(query);
   }
 
   @ApiOperation({ summary: '更新数据' })
